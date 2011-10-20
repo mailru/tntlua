99b14769954a6cfad53f8976e88a7a4c02db6160
Konstantin Osipov <kostja.osipov@gmail.com>
2011-10-20 14:39:19 +0400

    notifications.lua: support uniqueness in notification ids

--- notifications.lua
+++ notifications.lua
@@ -26,10 +26,10 @@
 --
 -- notification_add(user_id, fifo_no, notification_id)
 -- - pushes a notification into the fifo queue associated with the
---   type, incrementing the count of unread notifications associated
---   with the given fifo, as well as the total count
---   However, if unread_count is above fifo_max, counters
---   are not incremented
+--   type, unless it's already there, incrementing the count of unread
+--   notifications associated  with the given fifo, as well as the total
+--   count. However, if unread_count is above fifo_max, counters
+--   are not incremented.
 --
 -- notification_read(user_id, fifo_no)
 -- - read the notifications in the given fifo.
@@ -39,6 +39,17 @@
 -- notification_total_unread_count(user_id)
 -- - gets the total unread count
 --
+--  Removing duplicates
+--  -------------------
+--
+--  On top of just maintaining FIFO queues for each type,
+--  this set of Lua procedures ensures that each notification
+--  in each FIFO is unique. When inserting a new notification id,
+--  the FIFO is checked first whether the same id is already there.
+--  If it's present in the queue, it's simply moved to the
+--  beginning of the line, no duplicate is inserted.
+--  Unread counters are incremented only if the old id was
+--  already read.
 
 -- namespace which stores all notifications
 local space_no = 0
@@ -46,7 +57,7 @@ local space_no = 0
 local type_max = 5
 -- how many notifications of each type the associated
 -- FIFO keeps
-local fifo_max = 20 
+local fifo_max = 20
 -- field index of the first fifo in the typle.
 -- field 0 - user id, field 1 - total unread count, hence
 local fifo0_fieldno = 2
@@ -79,6 +90,24 @@ function notification_unread_count(user_id, fifo_no)
     fieldno = fifo0_fieldno + tonumber(fifo_no) * 2
     return box.unpack('i', notification_find_or_insert(user_id)[fieldno])
 end
+
+--
+-- Find if id is already present in the given fifo
+-- return true and id offset when found,
+-- false len(fifo) when not found
+--
+local function is_duplicate(fifo, id)
+    for i=1, #fifo, #id do
+        if string.sub(fifo, i, i+#id-1) == id then
+            -- we're going to insert a new notifiation,
+            -- return the old notification offset relative to
+            -- the new field size
+            return true, i - 1 + #id
+        end
+    end
+    return false, fifo_max * #id
+end
+
 --
 -- Append a notification to its fifo (one fifo per notification type)
 -- Update 'unread' counters, unless they are already at their max.
@@ -88,11 +117,20 @@ function notification_push(user_id, fifo_no, id)
     -- insert the new id at the beginning of fifo, truncate the tail
     -- use box SPLICE operation for that
     format = ":p:p"
-    args = { fieldno + 1, box.pack("ppp", 0, 0, id),
-             fieldno + 1, box.pack("ppp", fifo_max * #id, #id, "") }
+    args = { fieldno + 1, box.pack("ppp", 0, 0, id), fieldno + 1 }
+    --
+    -- check whether a duplicate id is already present
+    --
+    tuple = notification_find_or_insert(user_id)
+    _, dup_offset = is_duplicate(tuple[fieldno+1], id)
+    --
+    -- if duplicate is there, dup_offset points at it.
+    -- otherwise it points at fifo tail
+    --
+    table.insert(args, box.pack("ppp", dup_offset, #id, ""))
+    unread_count = box.unpack('i', tuple[fieldno])
     -- check if the counters need to be updated
-    unread_count = notification_unread_count(user_id, fifo_no)
-    if unread_count < fifo_max then
+    if unread_count < fifo_max and dup_offset > unread_count * #id then
     -- prepend ++total_unread_count, ++unread_count to our update operation
         format = "+p+p"..format
         args1 = args
