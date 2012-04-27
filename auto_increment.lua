7f1df8feba5f8aa4d40cb09fbb7975ef42700657
Konstantin Shulgin <konstantin.shulgin@gmail.com>
2012-04-27 15:56:57 +0400

    auto_increment:
    Converting spaceno to number was added. Variable in the function new defined as
    local, it allows to avoid problem with tarantool Lua global context.

--- auto_increment.lua
+++ auto_increment.lua
@@ -2,11 +2,11 @@
 -- inserts a tuple after getting the next value of the
 -- primary key and returns it back to the user
 function box.auto_increment(spaceno, ...)
-    max_tuple = box.space[spaceno].index[0].idx:max()
+    spaceno = tonumber(spaceno)
+    local max_tuple = box.space[spaceno].index[0].idx:max()
+    local max = -1
     if max_tuple ~= nil then
         max = box.unpack('i', max_tuple[0])
-    else
-        max = -1
     end
     return box.insert(spaceno, max + 1, ...)
 end
