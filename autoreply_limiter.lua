5922d3f71fb8084d4b279a82798c04757d65869c
Alexey Dolgavin <a.dolgavin@corp.mail.ru>
2012-11-29 17:46:36 +0400

    Added autoreply_limiter.lua

---
+++ autoreply_limiter.lua
@@ -0,0 +1,25 @@
+
+-- autoreply_limiter.lua
+-- Developed by Mail.Ru
+--
+
+local limiter_period = 4*24*3600
+
+function autoreply_limiter_check(userid, email)
+  userid = box.unpack('i', userid)
+  time   = os.time(); 
+
+  local tuple = box.select(0, 0, userid, email)
+  if tuple == nil then
+    box.insert(0, userid, email, time)
+    return 1
+  else
+    local timeOld = box.unpack('i', tuple[2])
+    if (time - timeOld) >= limiter_period then
+      box.replace(0, userid, email, time)
+      return 1
+    else
+      return 0
+    end
+  end
+end
