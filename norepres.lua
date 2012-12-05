a1ba8e4a8da250dd2ef33162aa187d0ab6050cf7
Dmitry Isaikin <isaikin@corp.mail.ru>
2012-12-05 14:19:27 +0400

    norepres.lua: add utility functions for selecting old tuples and deleting tuple by multi-part key

--- norepres.lua
+++ norepres.lua
@@ -1,6 +1,12 @@
 -- norepres.lua
 
 --
+-- Tuple: userid (NUM), digest (STR), uidl (NUM64), time (NUM)
+-- Index0: userid, digest. (TREE)
+-- Index1: time. (TREE)
+--
+
+--
 -- Delete all noreply reminders for user @userid.
 -- Returns flag of deletion success.
 --
@@ -14,4 +20,30 @@ function norepres_delall(userid)
         end
 
         return box.pack('i', #tuples)
+end
+
+function norepres_del(userid, digest)
+        -- client sends integers encoded as BER-strings
+        userid = box.unpack('i', userid)
+
+        local tuple = box.delete(0, userid, digest)
+        if tuple ~= nil then return 1 else return 0 end
+end
+
+function norepres_getold(time, limit)
+        -- client sends integers encoded as BER-strings
+	time = box.unpack('i', time)
+	limit = box.unpack('i', limit)
+
+	local tuples = { box.select_range(0, 1, limit) }
+
+	local ret = {}
+	for _, tuple in pairs(tuples) do
+		if box.unpack('i', tuple[3]) < time then
+		    table.insert(ret, { tuple[0], tuple[1],
+		                    box.unpack('i', string.sub(tuple[2], 0, 4)),
+		                    box.unpack('i', string.sub(tuple[2], 5, 8)) })
+		end
+	end
+	return unpack(ret)
 end
