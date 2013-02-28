66971f84c6a062bb1e2164dd12682648a6a259fd
Dmitry Isaikin <isaikin@corp.mail.ru>
2013-02-28 14:42:04 +0400

    list_mailer.lua: list_mailer_get_ext returns the subject prefix of user group in first tuple

--- list_mailer.lua
+++ list_mailer.lua
@@ -1,5 +1,9 @@
 
+-- space 0:
 -- tuple : (listid, email)
+-- space 1:
+-- tuple : (listid, subj_prefix)
+
 -- function returns list of mailer
 
 function list_mailer_get(listid)
@@ -15,4 +19,24 @@ function list_mailer_get(listid)
     end
     return unpack(result)
   end
+end
+
+function list_mailer_get_ext(listid)
+	listid = box.unpack('i', listid)
+
+	local properties = box.select(1, 0, listid)
+
+	local subj_prefix = ""
+	if properties ~= nil then subj_prefix = properties[1] end
+
+	local result = { subj_prefix }
+
+	local tuples = { box.select(0, 0, listid) }
+	if tuples == nil then return end
+
+	for _, tuple in pairs(tuples) do
+		table.insert(result, tuple[1])
+	end
+
+	return unpack(result)
 end
