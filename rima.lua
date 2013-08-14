f5d8271b81ddca84a52ccf77184054833d162131
Dmitry Isaikin <isaikin@corp.mail.ru>
2013-08-14 14:13:58 +0400

    rima.lua: rewrite priority and lock logic

--- rima.lua
+++ rima.lua
@@ -16,31 +16,30 @@
 
 --
 -- Space 0: Remote IMAP Collector Task Queue
---   Tuple: { unique_id (NUM64), key (STR), task_description (NUM) }
---   Index 0: TREE { unique_id }
---   Index 1: TREE { key, unique_id }
---
--- Space 1: Locked Keys
---   Tuple: { key (STR), lock_time (NUM) }
---   Index 0: TREE { key }
+--   Tuple: { task_id (NUM64), key (STR), task_description (NUM) }
+--   Index 0: TREE { task_id }
+--   Index 1: TREE { key, task_id }
 --
 -- Space 2: Task Priority
---   Tuple: { key (STR), priority (NUM) }
+--   Tuple: { key (STR), priority (NUM), is_locked (NUM), lock_time (NUM) }
 --   Index 0: TREE { key }
---   Index 1: TREE { priority }
+--   Index 1: TREE { priority, is_locked }
 --
 
-math.randomseed(box.time())
+local no_priority = 4294967295
 
 --
 -- Put task to the queue.
 --
 local function rima_put_impl(key, data, prio)
+	-- insert task data into the queue
 	box.auto_increment(0, key, data)
-
-	local tuple = box.select(2, 0, key)
-	if tuple == nil or ( #tuple > 1 and box.unpack('i', tuple[1]) < prio ) then
-		box.replace(2, key, prio)
+	-- increase priority of the key
+	local pr = box.select(2, 0, key)
+	if pr == nil then
+		box.insert(2, key, prio, 0, box.time())
+	elseif box.unpack('i', pr[1]) < prio or box.unpack('i', pr[1]) == no_priority then
+		box.update(2, key, "=p", 1, prio)
 	end
 end
 
@@ -58,51 +57,22 @@ function rima_put_with_prio(key, data, prio)
 	rima_put_impl(key, data, prio)
 end
 
-local function get_random_key()
-	for i = 1,5 do
-		local v = box.space[0].index[0]:random(math.random(4294967296))
-		if v == nil then return nil end -- no tuples in index
-		local key = v[1]
-		local exists = box.select(1, 0, key)
-		if exists == nil then return key end
+local function get_prio_key(prio)
+	for v in box.space[2].index[1]:iterator(box.index.EQ, prio, 0) do
+		local key = v[0]
+		-- lock the key and set prio to no_priority
+		box.update(2, key, "=p=p=p", 1, no_priority, 2, 1, 3, box.time())
+		return key
 	end
 	return nil
-end
-
-local function get_prio_key_impl(prio)
-	for i = 1,10 do
-		local v = box.space[0].index[0]:random(math.random(4294967296))
-		if v == nil then return nil end -- no tuples in index
-		local key = v[1]
-		local exists = box.select(1, 0, key)
-		if exists == nil then
-			local pr = box.select(2, 0, key)
-			if pr ~= nil and box.unpack('i', pr[1]) == prio then
-				return key
-			end
-		end
-	end
-	return nil
-end
-
-local function get_prio_key()
-	return nil
-end
-
-local function get_prio_key_eq(prio)
-	return get_prio_key_impl(prio)
 end
 
 local function get_key_data(key)
-	local time = os.time()
-	local status, _ = pcall(box.insert, 1, key, time)
-	if not status then return nil end
-
-	box.delete(2, key)
+	if key == nil then return nil end
 
 	local result = {}
 
-	local tuples = { box.select_limit(0, 1, 0, 1000, key) }
+	local tuples = { box.select_limit(0, 1, 0, 8000, key) }
 	for _, tuple in pairs(tuples) do
 		tuple = box.delete(0, tuple[0])
 		if tuple ~= nil then table.insert(result, tuple[2]) end
@@ -111,48 +81,21 @@ local function get_key_data(key)
 	return result
 end
 
-local function rima_get_impl()
-	local key = get_prio_key()
-	if key == nil then key = get_random_key() end
-	if key == nil then return true, nil, nil end
-
-	local result = get_key_data(key)
-	if result == nil then return false, nil, nil end
-	return true, key, result
-end
-
 local function rima_get_prio_impl(prio)
-	local key = get_prio_key_eq(prio)
-	if key == nil then return true, nil, nil end
-
+	local key = get_prio_key(prio)
 	local result = get_key_data(key)
-	if result == nil then return false, nil, nil end
-	return true, key, result
+	return key, result
 end
 
 --
 -- Request tasks from the queue.
 --
-function rima_get() -- deprecated
-	for i = 1,10 do
-		local status, key, result = rima_get_impl()
-		if status then
-			if result == nil then return end
-			return key, unpack(result)
-		end
-		box.fiber.sleep(0.001)
-	end
-end
-
 function rima_get_with_prio(prio)
 	prio = box.unpack('i', prio)
 
 	for i = 1,10 do
-		local status, key, result = rima_get_prio_impl(prio)
-		if status then
-			if result == nil then return end
-			return key, unpack(result)
-		end
+		local key, result = rima_get_prio_impl(prio)
+		if key ~= nil then return key, unpack(result) end
 		box.fiber.sleep(0.001)
 	end
 end
@@ -162,7 +105,15 @@ end
 -- Rima unlocks key and next rima_get() may returns tasks with such key.
 --
 function rima_done(key)
-	box.delete(1, key)
+	local pr = box.select(2, 0, key)
+	if pr == nil then return end
+
+	if box.unpack('i', pr[1]) == no_priority then
+		-- no tasks added while key was locked
+		box.delete(2, key)
+	else
+		box.update(2, key, "=p=p", 2, 0, 3, box.time())
+	end
 end
 
 --
@@ -173,16 +124,20 @@ local function is_expired(args, tuple)
 	if tuple == nil or #tuple <= args.fieldno then
 		return nil
 	end
+
+	-- expire only locked keys
+	if box.unpack('i', tuple[2]) == 0 then return false end
+
 	local field = tuple[args.fieldno]
-	local current_time = os.time()
+	local current_time = box.time()
 	local tuple_expire_time = box.unpack('i', field) + args.expiration_time
 	return current_time >= tuple_expire_time
 end
 
 local function delete_expired(spaceno, args, tuple)
-	box.delete(spaceno, tuple[0])
+	rima_done(tuple[0])
 end
 
 dofile('expirationd.lua')
 
-expirationd.run_task('expire_locks', 1, is_expired, delete_expired, {fieldno = 1, expiration_time = 30*60})
+expirationd.run_task('expire_locks', 2, is_expired, delete_expired, {fieldno = 3, expiration_time = 30*60})
