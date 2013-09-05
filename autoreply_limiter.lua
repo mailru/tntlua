527cc528d2a4d3834dcbfafe645b62485f013b31
Dmitry Isaikin <isaikin@corp.mail.ru>
2013-09-05 15:08:18 +0400

    autoreply_limiter.lua: add expiration of tuples

--- autoreply_limiter.lua
+++ autoreply_limiter.lua
@@ -11,7 +11,7 @@ local limiter_period = 12*60*60
 
 function autoreply_limiter_check(userid, email)
   userid = box.unpack('i', userid)
-  local time = os.time()
+  local time = box.time()
 
   local tuple = box.select(0, 0, userid, email)
   if tuple == nil then
@@ -27,3 +27,26 @@ function autoreply_limiter_check(userid, email)
     end
   end
 end
+
+--
+-- Run expiration of tuples
+--
+
+local function is_expired(args, tuple)
+	if tuple == nil or #tuple <= args.fieldno then
+		return nil
+	end
+
+	local field = tuple[args.fieldno]
+	local current_time = box.time()
+	local tuple_expire_time = box.unpack('i', field) + args.expiration_time
+	return current_time >= tuple_expire_time
+end
+
+local function delete_expired(spaceno, args, tuple)
+	box.delete(0, box.unpack('i', tuple[0]), tuple[1])
+end
+
+dofile('expirationd.lua')
+
+expirationd.run_task('expire_limits', 0, is_expired, delete_expired, {fieldno = 2, expiration_time = limiter_period})
