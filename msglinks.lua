916eddf2bbe8600c90a6eb5d0d3ad602209bd8a8
Dmitry Isaikin <isaikin@corp.mail.ru>
2014-02-24 20:42:15 +0400

    msglinks.lua: add expiration of links

--- msglinks.lua
+++ msglinks.lua
@@ -15,3 +15,26 @@ function msglinks_set(userid, fromid, linkid, toid, created)
 
 	box.insert(0, userid, fromid, linkid, toid, created)
 end
+
+--
+-- Run expiration of tuples
+--
+
+local function is_expired(args, tuple)
+	if tuple == nil or #tuple <= args.fieldno then
+		return nil
+	end
+
+	local field = tuple[args.fieldno]
+	local current_time = box.time()
+	local tuple_expire_time = box.unpack('i', field) + args.expiration_time
+	return current_time >= tuple_expire_time
+end
+
+local function delete_expired(spaceno, args, tuple)
+	box.delete(0, tuple[0], tuple[3])
+end
+
+dofile('expirationd.lua')
+
+expirationd.run_task('expire_msglinks', 0, is_expired, delete_expired, {fieldno = 4, expiration_time = 30*24*3600})
