73f1325359821544ec2c9711f3850719f76c90ec
Dmitry Isaikin <isaikin@corp.mail.ru>
2015-03-31 20:33:21 +0300

    rico.lua: RImap COunters storage

---
+++ rico.lua
@@ -0,0 +1,61 @@
+--
+-- rico.lua
+--
+
+--
+-- Remote Imap collector COunters. Stores some scheduling information needed for collectors.
+--
+
+--
+-- Space 0: Stores last collect time, last success collect time and so on.
+--   Tuple: { coll_id (NUM), last_time (NUM), last_ok (NUM), old_threshold (NUM), last_fullsync (NUM), success_collects_num (NUM) }
+--   Index 0: HASH { coll_id }
+--
+
+function rico_get(coll_id)
+	coll_id = box.unpack('i', coll_id)
+	local t = box.select_limit(0, 0, 0, 1, coll_id)
+	if t ~=nil then return t:transform(0,1) end
+end
+
+function rico_reset(coll_id)
+	coll_id = box.unpack('i', coll_id)
+        box.replace(0, coll_id, box.time(), box.time(), box.time(), box.time(), 0)
+end
+
+function rico_update_success(coll_id, need_update_old_threshold, need_update_last_fullsync)
+	coll_id = box.unpack('i', coll_id)
+	need_update_old_threshold = box.unpack('i', need_update_old_threshold)
+	need_update_last_fullsync = box.unpack('i', need_update_last_fullsync)
+
+	local status, t
+	if need_update_old_threshold ~= 0 and need_update_last_fullsync ~= 0 then
+		status, t = pcall(box.update, 0, { coll_id }, "=p=p=p=p+p", 1, box.time(), 2, box.time(), 3, box.time(), 4, box.time(), 5, 1)
+	elseif need_update_old_threshold ~= 0 then
+		status, t = pcall(box.update, 0, { coll_id }, "=p=p=p+p", 1, box.time(), 2, box.time(), 3, box.time(), 5, 1)
+	elseif need_update_last_fullsync ~= 0 then
+		status, t = pcall(box.update, 0, { coll_id }, "=p=p=p+p", 1, box.time(), 2, box.time(), 4, box.time(), 5, 1)
+	else
+		status, t = pcall(box.update, 0, { coll_id }, "=p=p+p", 1, box.time(), 2, box.time(), 5, 1)
+	end
+
+	if not status or t == nil then
+		box.replace(0, coll_id, box.time(), box.time(), box.time(), box.time(), 1)
+	end
+end
+
+function rico_update_failure(coll_id, need_update_last_fullsync)
+	coll_id = box.unpack('i', coll_id)
+	need_update_last_fullsync = box.unpack('i', need_update_last_fullsync)
+
+	local status, t
+	if need_update_last_fullsync ~= 0 then
+		status, t = pcall(box.update, 0, { coll_id }, "=p=p", 1, box.time(), 4, box.time())
+	else
+		status, t = pcall(box.update, 0, { coll_id }, "=p", 1, box.time())
+	end
+
+	if not status or t == nil then
+		box.replace(0, coll_id, box.time(), box.time(), box.time(), box.time(), 0)
+	end
+end
