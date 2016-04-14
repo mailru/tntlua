84372a6c3aa3f11d3af7c97f23f9701d9ea949ca
Ivan Remen <i.remen@corp.mail.ru>
2016-04-14 08:11:56 -0400

    Bugs fixed

--- pdd.lua
+++ pdd.lua
@@ -3,7 +3,13 @@ function get_mxstatus(pdd_domain)
     if not tup then
         return {0, 0}
     end
-    return {1, tup[1]}
+
+    local flags = 0
+    if #tup >= 2 then
+        flags = tup[1]
+    end
+
+    return {1, flags}
 end
 
 function get_pdd(pdd_domain)
@@ -11,5 +17,16 @@ function get_pdd(pdd_domain)
     if not tup then
         return {0, 0, ''}
     end
-    return {1, tup[1], tup[2]}
+
+    local flags = 0
+    if #tup >= 2 then
+        flags = tup[1]
+    end
+
+    local defuser = ''
+    if #tup >= 3 then
+        defuser = tup[2]
+    end
+
+    return {1, flags, defuser}
 end
