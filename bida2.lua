9ef05f68bda5aeb393104d11dfa10ff9884488f6
Pavel Berezhnoy <p.berezhnoy@corp.mail.ru>
2016-04-20 19:20:57 +0300

    bida-2: comment added

--- bida2.lua
+++ bida2.lua
@@ -15,6 +15,16 @@ function bida2_get_users_by_birthday(birthday, userid_offset)
 	birthday = box.unpack('i', birthday)
 	userid_offset = box.unpack('i', userid_offset)
 
+	--[[
+	We are using user id as an offset in out tarantool request.
+	From documentation:
+		localhost> lua box.select_range(4, 1, 2, '1')
+		---
+		 - '1': {'1'}
+		 - '2': {'2'}
+		...
+	That means, that select_range() function can return users with unexpected birthday => we should filter them out
+	--]]
 	local tuples = { box.select_range(0, 1, limit, birthday, userid_offset) }
 
 	local result = {}
